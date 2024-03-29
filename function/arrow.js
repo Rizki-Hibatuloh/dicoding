a85b0a1fee464375dbd457a0d740859a0c41821d
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-29 23:56:13 +0700

    function Expression and arrow function

--- function/arrow.js
+++ function/arrow.js
@@ -2,5 +2,25 @@
 
 function sayHello (greet){
     console.log(`${greet}, World!`); 
-}
+};
 sayHello("Good");
+
+
+//  FUNCTION EXPRESSION
+const sayName = function (name) {
+    console.log( `My name is ${name}` );
+};
+sayName("Nara");
+
+//  ARROW FUNCTION
+
+const city = (cityName) => {
+    console.log (`I live in ${cityName}`) ;
+}
+city ("Seoul")
+
+// PERSINGKAT CODE
+/*
+    const city= (cityName) => console.log(`I live in ${cityName}`);
+    city ("Seoul");
+*/
