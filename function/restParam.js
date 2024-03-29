2c4edfb1177c58dda955ad4e0311fe3924a41127
Rizki-Hibatuloh <rizkihibatuloh123@gmail.com>
2024-03-29 23:41:48 +0700

    function rest parameter

---
+++ function/restParam.js
@@ -0,0 +1,16 @@
+
+/* 
+    Rest parameter ini adalah kebalikan dari spread Operator.
+    Rest parameter juga dituliskan menggunakan three dots (...). Dengan
+    rest parameter,kita dapar menggabungkan beberapa elemen menjadi satu array.
+    contoh :
+*/
+
+function Sum(...numbers){
+    let result=0;
+    for(let i of numbers) {
+        result += i;
+    }
+    return result;
+}
+console.log(Sum(1,2,3,4,5)); // 15 
